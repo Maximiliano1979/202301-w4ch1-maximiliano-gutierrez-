390d39e908b64203b781f0bc579a472e5a8f628d
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-01-30 21:54:02 +0100

    Add .lintstagedrc.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Gentlemen } from "./types";
 
-function App() {
+const App = (): JSX.Element => {
+  const [gentlemen] = useState<Gentlemen[]>([
+    {
+      id: 1,
+      name: "Bertin Osborne",
+      status: "Alive",
+      profession: "Youtuber",
+      twitter: "@osbourne",
+      picture: "bertin.jpg",
+      alternativeText: "Osbourne pointing at you",
+      selected: true,
+    },
+    {
+      id: 2,
+      name: "The Farytale",
+      status: "RIP",
+      profession: "Influencer",
+      twitter: "pending",
+      picture: "fary.jpg",
+      alternativeText: "The Fary pointing at you",
+      selected: false,
+    },
+    {
+      id: 3,
+      name: "Julius Churchs",
+      status: "Alive",
+      profession: "Java developer",
+      twitter: "@julius_churchs",
+      picture: "julio.jpg",
+      alternativeText: "Churchs pointing at you",
+      selected: true,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <ul>
+        {gentlemen.map((gentleman) => (
+          <li>
+            <article>
+              <h3> {gentleman.name}</h3>
+              <span>{gentleman.status}</span>
+              <span>{gentleman.profession}</span>
+              <span>{gentleman.twitter}</span>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
